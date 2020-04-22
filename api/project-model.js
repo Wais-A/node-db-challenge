edc9c63a4bd121699f72d52bc10a33796d53d7aa
Wais <wais.almakaleh@gmail.com>
2020-04-21 22:16:41 -0400

    migrations added

--- api/project-model.js
+++ api/project-model.js
@@ -2,9 +2,27 @@ const db = require('../data/dbConfig');
 
 module.exports = {
  getProjects,
+ getProjectsById,
  getTasks,
+ getTasksById,
  getResources,
+ getResourcesById,
  addProjects,
+ addProjectsById,
  addTask,
- addResources
+ addResources,
+ remove
+}
+function getProjects(){}
+function getProjectsById(id){}
+function getTasks(){}
+function getTasksById(id){}
+function getResources(){}
+function getResourcesById(id){}
+function addProjects(insert){}
+function addProjectsById(){}
+function addTask(insert){}
+function addResources(insert){}
+function remove(id) {
+ 
 }
